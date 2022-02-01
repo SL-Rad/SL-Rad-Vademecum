6060e0224ec4a2156fae4247827c2236fad89f04
GeorgeCeladon <64736051+GeorgeCeladon@users.noreply.github.com>
2022-02-01 19:00:03 +0100

    anonimizzatore

--- lab/pydicom_snippets/dicom_patient_id.py
+++ lab/pydicom_snippets/dicom_patient_id.py
@@ -3,4 +3,4 @@ from pydicom import dcmread
 ds = dcmread("lab\python_dicom_anonymization\dicom\DICOM_sample.DCM")
 # Edit the (0010,0020) 'Patient ID' element
 ds.PatientID = "12345678"
-ds.save_as("lab\python_dicom_anonymization\dicom\DICOM_sample.DCM")
+ds.save_as("lab\python_dicom_anonymization\dicom\DICOM_sample2.DCM")
--- lab/python_dicom_anonymization/anonymize_dicom_by_web.py
+++ lab/python_dicom_anonymization/anonymize_dicom_by_web.py
@@ -1,101 +1,12 @@
-"""
-====================
-Anonymize DICOM data
-====================
+# Importa le library
+import numpy as np
+from pydicom import dcmread
 
-This example is a starting point to anonymize DICOM data.
+# Definisci dove si trova il file
+DICOM_file_path = 'lab\python_dicom_anonymization\dicom\DICOM_sample2.DCM'
 
-It shows how to read data and replace tags: person names, patient id,
-optionally remove curves and private tags, and write the results in a new file.
+# Definisci il file
+DICOM_file = dcmread(DICOM_file_path)
 
-"""
-
-# authors : Guillaume Lemaitre <g.lemaitre58@gmail.com>
-# license : MIT
-
-
-from pickletools import anyobject
-import tempfile
-
-import pydicom
-from pydicom.data import get_testdata_file
-
-print(__doc__)
-
-###############################################################################
-# Anonymize a single file
-###############################################################################
-
-filename = 'lab\python_dicom_anonymization\dicom\ddd.DCM'
-dataset = pydicom.dcmread(filename)
-
-data_elements = ['PatientID',
-                 'PatientBirthDate']
-for de in data_elements:
-    print(dataset.data_element(de))
-
-###############################################################################
-# We can define a callback function to find all tags corresponding to a person
-# names inside the dataset. We can also define a callback function to remove
-# curves tags.
-
-
-def person_names_callback(dataset, data_element):
-    if data_element.VR == "PN":
-        data_element.value = "anonymous"
-
-
-def curves_callback(dataset, data_element):
-    if data_element.tag.group & 0xFF00 == 0x5000:
-        del dataset[data_element.tag]
-
-
-###############################################################################
-# We can use the different callback function to iterate through the dataset but
-# also some other tags such that patient ID, etc.
-
-dataset.PatientID = "id"
-dataset.walk(person_names_callback)
-dataset.walk(curves_callback)
-
-###############################################################################
-# pydicom allows to remove private tags using ``remove_private_tags`` method
-
-dataset.remove_private_tags()
-
-###############################################################################
-# Data elements of type 3 (optional) can be easily deleted using ``del`` or
-# ``delattr``.
-
-if 'OtherPatientIDs' in dataset:
-    delattr(dataset, 'OtherPatientIDs')
-
-if 'OtherPatientIDsSequence' in dataset:
-    del dataset.OtherPatientIDsSequence
-
-###############################################################################
-# For data elements of type 2, this is possible to blank it by assigning a
-# blank string.
-
-tag = 'PatientBirthDate'
-if tag in dataset:
-    dataset.data_element(tag).value = '19000101'
-
-##############################################################################
-# Finally, this is possible to store the image
-
-data_elements = ['PatientID',
-                 'PatientBirthDate']
-for de in data_elements:
-    print(dataset.data_element(de))
-
-output_filename = tempfile.NamedTemporaryFile().name
-dataset.save_as(output_filename)
-
-
-###
-
-anonym = dataset.remove_private_tags()
-print(anonym)
-with open(filename, 'w') as i:
-    i.write(anonym)
+# Apri il file Mostra le informazioni dentro il file dicom
+print(DICOM_file)
