2917e0120b71236a2477925b9c824c5df31517a0
GeorgeCeladon <64736051+GeorgeCeladon@users.noreply.github.com>
2022-02-02 09:24:38 +0100

    anonimizzarore txt & dicom

--- lab/pydicom_snippets/open_dicom_data_via_pydicom.py
+++ lab/pydicom_snippets/open_dicom_data_via_pydicom.py
@@ -14,24 +14,3 @@ DICOM_file = dcmread(DICOM_file_path)
 
 # Apri il file Mostra le informazioni dentro il file dicom
 print(DICOM_file)
-
-###########################################################
-
-
-ds = dcmread("lab\python_dicom_anonymization\dicom\DICOM_sample.DCM")
-# Edit the (0010,0020) 'Patient ID' element
-ds.PatientID = "12345678"
-ds.save_as("lab\python_dicom_anonymization\dicom\DICOM_sample2.DCM")
-
-###########################################################
-
-# Importa le library
-
-# Definisci dove si trova il file
-DICOM_file_path = 'lab\python_dicom_anonymization\dicom\DICOM_sample2.DCM'
-
-# Definisci il file
-DICOM_file = dcmread(DICOM_file_path)
-
-# Apri il file Mostra le informazioni dentro il file dicom
-print(DICOM_file)
